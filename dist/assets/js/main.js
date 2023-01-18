95ae3a4850979e761797d4aa635ed972f78b64ad
Bakhriev <legendofpain5112@gmail.com>
2023-01-18 23:13:29 +0500

    complete advantage section

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,15 +1,18 @@
 function burgerMenu() {
 	const burger = document.querySelector(".burger")
 	const menu = document.querySelector(".menu")
+	const body = document.querySelector("body")
 
 	burger.addEventListener("click", () => {
 		menu.classList.toggle("active")
 		burger.classList.toggle("active")
+		body.classList.toggle("locked")
 	})
 	window.addEventListener("resize", () => {
 		if (window.innerWidth > 991.98) {
 			menu.classList.remove("active")
 			burger.classList.remove("active")
+			body.classList.remove("locked")
 		}
 	})
 }
